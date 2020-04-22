9a83bbaf3cd7727795d96cc963b6ab3b9ff68fd0
masterful-array-3850 <crgammage@gmail.com>
2020-04-22 17:28:00 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,5 +1,6 @@
 def find_element_index(array, value_to_find)
   count = 0
-  while count < array.length do |value_to_find|
+  while count < array.length
+    puts array[count] = value_to_find
 
 end
